eb558c908b9ec8e8ef54cad80a7b783fda65dda6
Priit Kallas <kallaspriit@gmail.com>
2018-04-18 14:26:50 +0300

    Updated typescript config.

--- build/src/storage/typeorm/index.js
+++ build/src/storage/typeorm/index.js
@@ -13,6 +13,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
@@ -55,25 +58,32 @@ var Migration = /** @class */ (function () {
     function Migration() {
     }
     __decorate([
-        typeorm_1.PrimaryColumn({ type: "varchar", nullable: false, length: 256 })
+        typeorm_1.PrimaryColumn({ type: "varchar", nullable: false, length: 256 }),
+        __metadata("design:type", String)
     ], Migration.prototype, "name", void 0);
     __decorate([
-        typeorm_1.Column({ type: "varchar", nullable: true })
+        typeorm_1.Column({ type: "varchar", nullable: true }),
+        __metadata("design:type", String)
     ], Migration.prototype, "filename", void 0);
     __decorate([
-        typeorm_1.Column({ type: "varchar", nullable: false, default: common_1.MigrationStatus.RUNNING })
+        typeorm_1.Column({ type: "varchar", nullable: false, default: common_1.MigrationStatus.RUNNING }),
+        __metadata("design:type", String)
     ], Migration.prototype, "status", void 0);
     __decorate([
-        typeorm_1.Column({ type: "int", nullable: true })
+        typeorm_1.Column({ type: "int", nullable: true }),
+        __metadata("design:type", Number)
     ], Migration.prototype, "timeTaken", void 0);
     __decorate([
-        typeorm_1.Column({ type: "text", nullable: true })
+        typeorm_1.Column({ type: "text", nullable: true }),
+        __metadata("design:type", String)
     ], Migration.prototype, "result", void 0);
     __decorate([
-        typeorm_1.CreateDateColumn()
+        typeorm_1.CreateDateColumn(),
+        __metadata("design:type", Date)
     ], Migration.prototype, "startDate", void 0);
     __decorate([
-        typeorm_1.UpdateDateColumn()
+        typeorm_1.UpdateDateColumn(),
+        __metadata("design:type", Date)
     ], Migration.prototype, "endDate", void 0);
     Migration = __decorate([
         typeorm_1.Entity()
